a701f7e48af13d7873532c137ea951f3733c210d
uwalakab <62111763+uwalakab@users.noreply.github.com>
2021-07-21 19:50:51 +0100

    Update backup-config-vol.sh

--- backup-config-vol.sh
+++ backup-config-vol.sh
@@ -1,4 +1,4 @@
-# Backup the persistent data on the config volume
+# REFERENCE INFO: Backup the persistent data on the config volume
 # Use rsync to perform a differential mirror backup of files (only copy the files that have changed, and delete those that are no longer present in the source).
 
 # PWD = /share/homes/admin/crashplan-stuff (Need to us "sudo -i" command)
